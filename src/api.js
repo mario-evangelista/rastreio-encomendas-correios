b70e2907a8fc7a5ced2a8ebc235b37b396c242b2
Mário Evangelista <121322767+mario-evangelista@users.noreply.github.com>
2025-05-27 22:35:11 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", // URL do backend
+  //baseURL: "http://localhost:8080", // URL do backend
+  https://api-proxy-labs-wonca.onrender.com
 });
 
 export default api;
